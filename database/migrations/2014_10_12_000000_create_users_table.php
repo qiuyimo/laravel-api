78f4159a62f457979e702f2f182218f19923928b
qiuyu <530004000@qq.com>
2019-12-16 17:10:43 +0800

    创建用户表.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->comment('用户名称');
+            $table->string('password')->comment('密码');
+            $table->text('last_token')->comment('登录时的 token');
+            $table->tinyInteger('status')->comment('用户状态 -1代表已删除 0代表正常 1代表冻结');
             $table->timestamps();
         });
     }
